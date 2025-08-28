9850bed6a1f8f0e8f8e81e725e3fc3cf2496d9b4
Jefferson Olaguer <jeff.olaguer@gmail.com>
2025-08-28 05:05:29 +0000

    Task2 - 08-28-2025

---
+++ chapter_2/Task2.java
@@ -0,0 +1,20 @@
+public class Task2 {
+    public static void main(String[] args) {
+        
+        byte zero = 0;
+        int e;
+        int h = 0xa;
+        e = 310*h + 10;
+        boolean x = false;
+        x = !x;
+
+        String output1 = "H"+ e + " ";
+        String output2 = "w" + zero + "r" + ++zero + "d" + " ";
+        String output3 = ++zero + "." + (zero-2) + " ";
+        System.out.println();
+        System.out.print(output1 + output2 + output3 + x);
+        System.out.println();
+
+
+    }
+}
