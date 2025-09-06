ce1555d77c2ea12f61210ae5f7c06e1760da97cf
Jefferson Olaguer <jeff.olaguer@gmail.com>
2025-09-06 02:53:23 +0000

    Day19 start

---
+++ chapter3/DatesAndTime.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+import java.time.*;
+
+public class DatesAndTime {
+
+    public static void main(String[] args) {
+        
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+
+    }
+}
