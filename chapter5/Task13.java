e1ead3db8bc30c3dde36cad0ea90b4fcaab3a728
Jefferson Olaguer <jeff.olaguer@gmail.com>
2025-10-03 03:45:58 +0000

    Task13

---
+++ chapter5/Task13.java
@@ -0,0 +1,41 @@
+package chapter5;
+
+class Vehicle {
+    String make;
+    String model;
+    int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayVehicleDetails() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
+
+// Derived class
+class Car extends Vehicle {
+    int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+    public void displayDetails() {
+        System.out.println("Car Details:");
+        super.displayVehicleDetails();
+        System.out.println("Number of Doors: " + numberOfDoors);
+    }
+}
+
+public class Task13 {
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022, 4);
+        myCar.displayDetails();
+    }
+}
